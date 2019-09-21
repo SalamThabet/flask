5b858abee1c9eeee2db41101f31bfa09bfebe347
Salam <salam.th.d@gmail.com>
2019-09-21 11:49:24 +0300

    ..

--- flaskApi.py
+++ flaskApi.py
@@ -6,16 +6,10 @@ Created on Sat Sep 14 19:32:26 2019
 """
 
 from flask import Flask
-from flask_restful import Api,Resource
-
 app = Flask(__name__)
-api = Api(app)
-
-class MLClassifier(Resource):
-    def get(self):
-        return{'label':'apple'}
-api.add_resource(MLClassifier,'/classifier')
-
+@app.route('/')
+def home():
+    return '<h1> Hello from flask </h1>'
 
 if __name__ == '__name__':
     app.run()
